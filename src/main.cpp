3e4e2d708fb4579f188c8e81955a339843cd7357
Andreas-Bugge <202204219@post.au.dk>
2022-10-13 16:32:27 +0200

    First edition

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+#include "taylor_sine.h"
+#include <stdio.h>
+#include <math.h>
 
 // Add your test cases for Exercise 1b,c) here
-int main(int argc, char **argv)
+int main()
 {
+    double x = 3.1415;
+    double n = 7;
+    printf("The sine of math is %lf\n", sin(x));
+    printf("The sine of taylor_sine is %lf\n",taylor_sine(x,n));
+    
     return 0;
 }
--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,7 +1,34 @@
 #include "taylor_sine.h"
+#include <math.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+int factorial (int n)
+{
+    assert(n >= 0); // checks to make sure n is higher than 0
+    if(n == 0) //edge case if n is 0
+    return 1;
+    int res = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+bool iseven(int n)
+{
+    return n%2 == 0;  
+}
 double taylor_sine(double x, int n)
 {
-    /* implement your function here */
-    
-    return -1.0;
+    double sum = 0;
+    int holder = 1;
+    for(int c = 0; c < n; c++)
+    {
+    double fortegn = iseven(c) ? 1.0 : -1.0;
+    sum += fortegn * pow(x, holder) / (double) factorial(holder);
+    holder += 2;
+    }
+
+    return sum;
 }
